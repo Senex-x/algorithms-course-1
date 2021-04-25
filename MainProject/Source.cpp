48082eb622c713606f105bc811cc097dfa82b9ed
V-Senex <junior.07.inator@gmail.com>
2021-04-25 16:16:12 +0300

    Code cleanup

--- MainProject/Source.cpp
+++ MainProject/Source.cpp
@@ -1,32 +1,39 @@
 #include<iostream>
+#include<string>
+#include<sstream>
 #include<fstream>
 
 using namespace std;
 
-struct node {
-	node* prev;
-	node* next;
-	node* child;
-	node* parent;
+struct Node {
+	Node* prev;
+	Node* next;
+	Node* child;
+	Node* parent;
 	int key;
 	int degree;
-	bool marked;
+	bool mark;
 };
 
 struct FibonacciHeap {
 	int size = 0;
-	node* min = NULL;	
+	Node* min = NULL;
 
-	node* insert(int key) {
-		node* ret = _singleton(key);
-		min = mergeTop(min, ret);
+	void insert(int key) {
+		Node* n = new Node();
+		n->key = key;
+		n->prev = n->next = n;
+		n->degree = 0;
+		n->mark = false;
+		n->child = NULL;
+		n->parent = NULL;
+		min = mergeTop(min, n);
 		size++;
-		return ret;
 	}
 
-	void merge(FibonacciHeap& other) {
-		min = mergeTop(min, other.min);
-		other.min = NULL;
+	void merge(FibonacciHeap& second) {
+		min = mergeTop(min, second.min);
+		second.min = NULL;
 	}
 
 	int getMin() {
@@ -34,16 +41,76 @@ struct FibonacciHeap {
 	}
 
 	int removeMin() {
-		node* old = min;
-		min = _removeMinimum(min);
-		int ret = old->key;
-		delete old;
+		Node* old = min;
+		int res = min->key;
+		if (min->child != NULL) {
+			Node* c = min->child;
+			do {
+				c->mark = false;
+				c->parent = NULL;
+				c = c->next;
+			} while (c != min->child);
+		}
+
+
+		if (min->next == min) {
+			min = min->child;
+		}
+		else {
+			min->next->prev = min->prev;
+			min->prev->next = min->next;
+			min = mergeTop(min->next, min->child);
+		}
+		if (min == NULL) return res;
+		Node* trees[64] = { NULL };
+
+		while (true) {
+			if (trees[min->degree] != NULL) {
+				Node* t = trees[min->degree];
+				if (t == min)break;
+				trees[min->degree] = NULL;
+				if (min->key < t->key) {
+					t->prev->next = t->next;
+					t->next->prev = t->prev;
+					addChild(min, t);
+				}
+				else {
+					t->prev->next = t->next;
+					t->next->prev = t->prev;
+					if (min->next == min) {
+						t->next = t->prev = t;
+						addChild(t, min);
+						min = t;
+					}
+					else {
+						min->prev->next = t;
+						min->next->prev = t;
+						t->next = min->next;
+						t->prev = min->prev;
+						addChild(t, min);
+						min = t;
+					}
+				}
+				continue;
+			}
+			else {
+				trees[min->degree] = min;
+			}
+			min = min->next;
+		}
+		Node* start = min;
+		do {
+			if (min->key < min->key)min = min;
+			min = min->next;
+		} while (min != start);
+
+
 		size--;
-		return ret;
+		return res;
 	}
 
-	void display(node* min) {
-		node* ptr = min;
+	void display(Node* min) {
+		Node* ptr = min;
 		if (ptr != NULL) {
 			do {
 				cout << ptr->key;
@@ -56,27 +123,16 @@ struct FibonacciHeap {
 		}
 	}
 
-	node* _singleton(int key) {
-		node* n = new node;
-		n->key = key;
-		n->prev = n->next = n;
-		n->degree = 0;
-		n->marked = false;
-		n->child = NULL;
-		n->parent = NULL;
-		return n;
-	}
-
-	node* mergeTop(node* a, node* b) {
+	Node* mergeTop(Node* a, Node* b) {
 		if (a == NULL)return b;
 		if (b == NULL)return a;
 		if (a->key > b->key) {
-			node* temp = a;
+			Node* temp = a;
 			a = b;
 			b = temp;
 		}
-		node* an = a->next;
-		node* bp = b->prev;
+		Node* an = a->next;
+		Node* bp = b->prev;
 		a->next = b;
 		b->prev = a;
 		an->prev = bp;
@@ -84,92 +140,14 @@ struct FibonacciHeap {
 		return a;
 	}
 
-	void _deleteAll(node* n) {
-		if (n != NULL) {
-			node* c = n;
-			do {
-				node* d = c;
-				c = c->next;
-				_deleteAll(d->child);
-				delete d;
-			} while (c != n);
-		}
-	}
-
-	void _addChild(node* parent, node* child) {
+	void addChild(Node* parent, Node* child) {
 		child->prev = child->next = child;
 		child->parent = parent;
 		parent->degree++;
 		parent->child = mergeTop(parent->child, child);
 	}
 
-	void _unMarkAndUnParentAll(node* n) {
-		if (n == NULL)return;
-		node* c = n;
-		do {
-			c->marked = false;
-			c->parent = NULL;
-			c = c->next;
-		} while (c != n);
-	}
-
-	node* _removeMinimum(node* n) {
-		_unMarkAndUnParentAll(n->child);
-		if (n->next == n) {
-			n = n->child;
-		}
-		else {
-			n->next->prev = n->prev;
-			n->prev->next = n->next;
-			n = mergeTop(n->next, n->child);
-		}
-		if (n == NULL)return n;
-		node* trees[64] = { NULL };
-
-		while (true) {
-			if (trees[n->degree] != NULL) {
-				node* t = trees[n->degree];
-				if (t == n)break;
-				trees[n->degree] = NULL;
-				if (n->key < t->key) {
-					t->prev->next = t->next;
-					t->next->prev = t->prev;
-					_addChild(n, t);
-				}
-				else {
-					t->prev->next = t->next;
-					t->next->prev = t->prev;
-					if (n->next == n) {
-						t->next = t->prev = t;
-						_addChild(t, n);
-						n = t;
-					}
-					else {
-						n->prev->next = t;
-						n->next->prev = t;
-						t->next = n->next;
-						t->prev = n->prev;
-						_addChild(t, n);
-						n = t;
-					}
-				}
-				continue;
-			}
-			else {
-				trees[n->degree] = n;
-			}
-			n = n->next;
-		}
-		node* min = n;
-		node* start = n;
-		do {
-			if (n->key < min->key)min = n;
-			n = n->next;
-		} while (n != start);
-		return min;
-	}
-
-	node* cut(node* min, node* n) {
+	Node* cut(Node* min, Node* n) {
 		if (n->next == n) {
 			n->parent->child = NULL;
 		}
@@ -179,25 +157,25 @@ struct FibonacciHeap {
 			n->parent->child = n->next;
 		}
 		n->next = n->prev = n;
-		n->marked = false;
+		n->mark = false;
 		return mergeTop(min, n);
 	}
 
-	node* decreaseKey(node* min, node* n, int key) {
+	Node* decreaseKey(Node* min, Node* n, int key) {
 		if (n->key < key)return min;
 		n->key = key;
 		if (n->parent) {
 			if (n->key < n->parent->key) {
 				min = cut(min, n);
-				node* parent = n->parent;
+				Node* parent = n->parent;
 				n->parent = NULL;
-				while (parent != NULL && parent->marked) {
+				while (parent != NULL && parent->mark) {
 					min = cut(min, parent);
 					n = parent;
 					parent = n->parent;
 					n->parent = NULL;
 				}
-				if (parent != NULL && parent->parent != NULL)parent->marked = true;
+				if (parent != NULL && parent->parent != NULL)parent->mark = true;
 			}
 		}
 		else {
@@ -209,32 +187,29 @@ struct FibonacciHeap {
 	}
 };
 
-#include<string>
-#include<cstring>
-#include<sstream>
-
-
 int main() {
 	FibonacciHeap* h = new FibonacciHeap();
 
-	
-
-	int size = 30000000;
+	int size = 20000000;
+	int step = 2000000;
 	int div = size / 100;
 
-	for (int i = 12000000; i < 20000000; i += 2000000) {
+	for (int i = step; i <= size; i += step) {
 		string path = "D:\\Projects\\Cpp\\tests\\seq-length-" + to_string(i) + ".txt";
 		ifstream file(path);
 		int s;
-		
+
 		file >> s;
-		double startTime = clock();
+
 		while (!file.eof()) {
 			h->insert(s);
 			file >> s;
 		}
+		double startTime = clock();
+		h->insert(s);
+
 		double addTime = clock();
-		cout << "add " << addTime << " ";
+		cout << "add " << addTime - startTime << " ";
 
 		h->removeMin();
 		double decTime = clock();
@@ -242,7 +217,7 @@ int main() {
 
 		h->decreaseKey(h->min, h->min->child, 0);
 		double endTime = clock();
-		cout << "dec " << endTime - decTime << endl;
+		cout << "decr " << endTime - decTime << endl;
 
 		file.close();
 	}
