81bae597fc0b697d4e8a725f49acde5b574938fe
V-Senex <junior.07.inator@gmail.com>
2021-05-18 19:18:33 +0300

    Sort update

--- TopologicSort/Source.cpp
+++ TopologicSort/Source.cpp
@@ -26,37 +26,48 @@ bool compare(pair<int, int> p1, pair<int, int> p2) {
 string path = "D:\\Projects\\Cpp\\tests\\seq.txt";
 
 int main() {
+	cout << "START" << endl;
 	int n, counter = 0;
 
 	ifstream file(path);
 	int s;
+	bool end = false;
 
 	while (!file.eof()) {
 		file >> s;
 		n = s;
-		cout << "Data set size: " << n << endl;
+		
+		double startTime = clock();
+
 		vector<vector<int>> matrix(n, vector<int>(n));
 		vector<bool> mark(n, false);
 		vector<pair<int, int>> result(n);
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
+				if(!file.eof())
 				file >> matrix[i][j];
+				else {
+					end = true;
+				}
 			}
 		}
-
+		if (end) break;
 		for (int i = 0; i < mark.size(); i++) {
 			if (!mark[i]) { // not marked yet
 				dfs(i, matrix, mark, counter, result);
 			}
 		}
 
+		cout << "SIZE: " << n << endl;
 		sort(result.begin(), result.end(), compare);
+		double endTime = clock();
+		cout << "TIME: " << endTime - startTime << endl;
 
-		for (int i = 0; i < result.size(); i++) {
+		/*for (int i = 0; i < result.size(); i++) {
 			cout << result[i].first << " ";
 		}
-		cout << endl;
+		cout << endl;*/
 	}
 }
 
